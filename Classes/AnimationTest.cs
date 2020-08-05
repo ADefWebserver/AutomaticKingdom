6600baaa8ecdd7967fdde840d1f385e79b0f822f
Michael Washington <Webmaster@ADefWebserver.com>
2020-08-05 11:50:50 -0700

    In progress on custom button

---
+++ Classes/AnimationTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using IronBlock;
+using IronBlock.Blocks;
+using IronBlock.Utils;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace AutomaticKingdom.Classes
+{
+    public class AnimationTest : IBlock
+    {
+        public List<string> Text { get; set; } = new List<string>();
+        public override object Evaluate(Context context)
+        {
+            // read a field
+            var AnimationSelection = this.Fields.Get("AnimationSelection");
+
+            //Console.WriteLine(AnimationSelection);
+
+            //return base.Evaluate(context);
+            return AnimationSelection;
+        }
+
+        public override SyntaxNode Generate(Context context)
+        {
+            var text = this.Fields.Get("AnimationSelection");
+
+            return LiteralExpression(
+                    SyntaxKind.StringLiteralExpression,
+                        Literal($"Animation = {text}")
+                    );
+        }
+    }
+}
