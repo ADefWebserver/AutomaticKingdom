b991037dfa40c34d1d9e3efeae5ee903bf118c49
Michael Washington <Webmaster@ADefWebserver.com>
2020-08-04 18:52:47 -0700

    Added custom button automatickingdom_animationtest

---
+++ wwwroot/javascript/automatickingdom_animationtest.js
@@ -0,0 +1,12 @@
+﻿Blockly.Blocks['automatickingdom_animationtest'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldLabelSerializable("Execute Animation"), "ExecuteAnimation")
+            .appendField(new Blockly.FieldDropdown([["TPose", "TPose"], ["Walking", "Walking"], ["Running", "Running"], ["Attack", "Attack"], ["Idle", "Idle"]]), "AnimationSelection");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("Choose Animation");
+        this.setHelpUrl("");
+    }
+};
